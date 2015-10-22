f949cc5fa9b06864239b062f14315e842651311f
Chris Manna <nitro7777@users.noreply.github.com>
2015-10-21 20:02:00 -0400

    updating

--- main.rb
+++ main.rb
@@ -1,5 +1,12 @@
-require_relative Mastermind_game.rb
+require_relative "Mastermind_game.rb"
 
 def main_method
-
+	a = Board.new()
+	puts a.get_total_pool_str()
+	puts a.set_color_code
+	puts "Input new guess: "
+	a.set_new_guess
+	a.check_placement
 end
+
+main_method
