e6f8c31d860b7390153da5996ae032c51baf583b
Chris Manna <nitro7777@users.noreply.github.com>
2015-10-22 12:15:47 -0400

    works properly
    This code works properly but needs to be refactored to allow for reverse roles.

--- board.rb
+++ board.rb
@@ -1,41 +1,42 @@
 class Board
 	TOTAL_POOL_ARR = ["RED","GREEN","BLUE", "YELLOW","BROWN","ORANGE","BLACK","WHITE"]
-	attr_accessor :guesses_left
+#	attr_accessor :guesses_left
 	def initialize()
 		@guesses_left = 12
 		@aligned = 0
 		@not_aligned = 0
 		@code_arr = []
 		@guess_arr = Array.new()
-		@choice
+		@choice = nil
 		@guess_arr = []
-		
 	end
 	
-	def get_total_pool_str()
-		TOTAL_POOL_ARR.join(", ")
+	def get_total_pool_str
+		puts TOTAL_POOL_ARR.join(", ")
 	end
 	
 	def set_color_code()
-			code_arr = TOTAL_POOL_ARR.sample(4)
+			@code_arr = TOTAL_POOL_ARR.sample(4)
 	end
 	
 	def set_new_guess()
 		4.times { @guess_arr << gets.chomp.upcase }
+		# find a way to guarantee and check this
+	end
+	
+	def set_aligneds
+		@aligned = 0
+		@not_aligned = 0
 	end
 	
 	def check_placement
 		@guess_arr[-4..-1].each_with_index do |elmt, idx|
 			if @code_arr[idx] == elmt
-				puts code_arr[idx]
+				@aligned += 1
+			elsif @code_arr.include?(elmt)
+				@not_aligned += 1
 			end
-			puts idx
-#				@aligned += 1
-#			elsif @code_arr.include?(elmt)
-#				@not_aligned += 1
-#			end
 		end
-#		set_guesses_left
 	end
 	
 	def set_guesses_left
@@ -51,33 +52,32 @@ class Board
 	def get_not_aligned
 		@not_aligned
 	end
+	
 	def get_board
-		puts "#{@get_aligned} colors properly lined up."
-		puts "#{get_not_aligned} colors not aligned but do exist in coded array."
-		puts "Guesser has #{get_guesses_left}"
-		get_guesses
-	end
-	def get_guesses
-		puts "here are your guesses"
-		guess_arr.each_with_index do |elmt, idx|
-			print idx % 4 == 0? "\n#{elmt}" : elmt 
-		end
 		get_outcome
+		
+		puts "#{@aligned} colors aligned."
+		puts "#{@not_aligned} colors not aligned."
+		puts "Guesser has #{get_guesses_left} guesses left"
 	end
+
+##########################################
 	def get_outcome
-		winner if get_aligned == 4
+		puts winner if get_aligned == 4
 		loser if get_guesses_left == 0
 	end
 	def winner
 		puts "You win!"
-		exit_game
+		puts exit_game
 	end
 	def loser
 		puts "You lose!"
 		exit_game
 	end
 	def exit_game
+		puts "The winning combination was: \n"
+		puts @code_arr.join(", ")
 		puts "Thanks for playing"
-		exit(0)
+		puts exit(0)
 	end
 end
